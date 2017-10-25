88c2bafdd641ff9a072d33cae35713ace6be9cc3
Apinun <apinun.wong@gmail.com>
2017-10-25 07:56:44 +0700

    commit

---
+++ TestGitNotPod/TestGitNotPod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGitNotPod
+//
+//  Created by Apinun Wongintawang on 10/25/17.
+//  Copyright © 2017 Apinun Wongintawang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
